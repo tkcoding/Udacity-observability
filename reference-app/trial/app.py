0f944d2c4b4101e74bf965954bcc34ebe800122f
TK Yeow <ludicroustk@gmail.com>
2022-03-28 09:14:54 +0800

    Adding trial appfor testing

--- reference-app/trial/app.py
+++ reference-app/trial/app.py
@@ -1,78 +1,122 @@
-from flask import Flask, render_template, request, jsonify
-from flask_cors import CORS
 import logging
+import re
+import requests
 
-from jaeger_client import Config
-from jaeger_client.metrics.prometheus import PrometheusMetricsFactory
-from prometheus_flask_exporter.multiprocess import GunicornInternalPrometheusMetrics
+
+from flask import Flask, jsonify, render_template, request
+
+from prometheus_flask_exporter import PrometheusMetrics
+from prometheus_flask_exporter.multiprocess import GunicornPrometheusMetrics
+
 from opentelemetry import trace
-from opentelemetry.exporter import jaeger
+from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import (
-    ConsoleSpanExporter,
-    SimpleExportSpanProcessor,
+
+app = Flask(__name__, template_folder='templates')
+
+# -- Monitoring: Define Monitoring metrics --
+metrics = PrometheusMetrics(app)
+# metrics = GunicornPrometheusMetrics(app)
+metrics.info("app_info", "Application info", version="1.0.3")
+# -- --Sample custom metrics (unused since there are no outgoing requests)
+record_requests_by_status = metrics.summary(
+        'requests_by_status', 'Request latencies by status',
+        labels={'status': lambda: request.status_code()}
+)
+record_page_visits = metrics.counter(
+    'invocation_by_type', 'Number of invocations by type',
+    labels={'item_type': lambda: request.view_args['type']}
 )
 
-trace.set_tracer_provider(TracerProvider())
-trace.get_tracer_provider().add_span_processor(
-    SimpleExportSpanProcessor(ConsoleSpanExporter())
-)
-
-app = Flask(__name__)
-RequestsInstrumentor().instrument()
+# -- Observability: Prep app for tracing -- 
 FlaskInstrumentor().instrument_app(app)
 RequestsInstrumentor().instrument()
-metrics = PrometheusMetrics(app)
-CORS(app)
-
-#config = Config(
-#        config={},
-#        service_name='your-app-name',
-#        validate=True,
-#        metrics_factory=PrometheusMetricsFactory(service_name_label='your-app-name')
-#)
-#tracer = config.initialize_tracer()
-
-def init_tracer(service):
-    logging.getLogger('').handlers = []
-    logging.basicConfig(format='%(message)s', level=logging.DEBUG)
-
-    config = Config(
-        config={
-            'sampler': {
-                'type': 'const',
-                'param': 1,
-            },
-            'logging': True,
-        },
-        service_name=service,
+# -- -- Configure Tracer 
+trace.set_tracer_provider(
+    TracerProvider(
+        resource=Resource.create({SERVICE_NAME: "trial-service"})  
     )
+)
+# -- -- Set Jaeger Exporter --
+jaeger_exporter = JaegerExporter(
+    # configure agent
+    agent_host_name='localhost',
+    agent_port=6831,
+    # optional: configure also collector
+    # collector_endpoint='http://localhost:14268/api/traces?format=jaeger.thrift',
+    # username=xxxx, # optional
+    # password=xxxx, # optional
+    # max_tag_value_length=None # optional
+)
 
-    # this call also sets opentracing.tracer
-    return config.initialize_tracer()
+# -- -- Create a BatchSpanProcessor and add the exporter to it --
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(jaeger_exporter)
+)
+# -- -- Initialize Tracer --
+tracer = trace.get_tracer(__name__)
 
-tracer = init_tracer('first-service')
+## -- Logging: Define logging parameters --
+logging.getLogger("").handlers = []
+logging.basicConfig(format="%(message)s", level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-@app.route('/')
+# -- Application Body: Routes and Logic --
+@app.route("/")
 def homepage():
     return render_template("main.html")
-    with tracer.start_span('get-python-jobs') as span:
-        homepages = []
-        res = requests.get('https://jobs.github.com/positions.json?description=python')
-        span.set_tag('first-tag', len(res.json()))
+
+@app.route("/trace")
+def trace():
+    def remove_tags(text):
+        tag = re.compile(r"<[^>]+>")
+        return tag.sub("", text)
+
+    with tracer.start_span("get-python-jobs") as span:
+        res = requests.get("https://jobs.github.com/positions.json?description=python")
+        span.log_kv({"event": "get jobs count", "count": len(res.json())})
+        span.set_tag("jobs-count", len(res.json()))
+
+        jobs_info = []
         for result in res.json():
-            try:
-                homepages.append(requests.get(result['company_url']))
-            except:
-                return "Unable to get site for %s" % result['company']
-        
+            jobs = {}
+            with tracer.start_span("request-site") as site_span:
+                logger.info(f"Getting website for {result['company']}")
+                try:
+                    jobs["description"] = remove_tags(result["description"])
+                    jobs["company"] = result["company"]
+                    jobs["company_url"] = result["company_url"]
+                    jobs["created_at"] = result["created_at"]
+                    jobs["how_to_apply"] = result["how_to_apply"]
+                    jobs["location"] = result["location"]
+                    jobs["title"] = result["title"]
+                    jobs["type"] = result["type"]
+                    jobs["url"] = result["url"]
 
+                    jobs_info.append(jobs)
+                    site_span.set_tag("http.status_code", res.status_code)
+                    site_span.set_tag("company-site", result["company"])
+                except Exception:
+                    logger.error(f"Unable to get site for {result['company']}")
+                    site_span.set_tag("http.status_code", res.status_code)
+                    site_span.set_tag("company-site", result["company"])
+    return jsonify(jobs_info)
 
-    return jsonify(homepages)
+@app.route("/success-response")
+def client_success_page():
+    return "Planned 200 response", 200
+
+@app.route("/client-error")
+def client_error_page():
+    return "Planned 400 error", 400
+
+@app.route("/server-error")
+def server_error_page():
+    return "Planned 500 error", 500
 
 if __name__ == "__main__":
-    app.run(debug=True,)
+    app.run(debug=True, host="0.0.0.0", port="8083")
